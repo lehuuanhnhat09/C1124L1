7bec2b077891e9254e094e3768c264f54090f1dd
le huu anh nhat <lehuuanhnhat09@gmail.com>
2024-12-12 21:20:30 +0700

    nop bai 6

---
+++ ss6_tong_quang_javaScript/thuc_hanh/hien_thi_dau_ra/thuc_hanh.js
@@ -0,0 +1,12 @@
+function show(){
+    alert('xinchao')
+}
+function tb(){
+    document.getElementById("tb").innerHTML = "hello";
+}
+function mes() {
+    document.write("hi");
+}
+function mas(){
+    console.log('xin chaoooo')
+}
