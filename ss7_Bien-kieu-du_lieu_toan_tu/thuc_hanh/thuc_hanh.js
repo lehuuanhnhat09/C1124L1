63d54a4f972ac1eb73de37afe38cb0bd89d1efcb
le huu anh nhat <lehuuanhnhat09@gmail.com>
2024-12-17 21:24:41 +0700

    nop bai 7

---
+++ ss7_Bien-kieu-du_lieu_toan_tu/thuc_hanh/thuc_hanh.js
@@ -0,0 +1,18 @@
+var tuoi; // khai bao bien//
+tuoi = 16; //gan gia tri,29 la so nguyen, = la toan tu gan
+console.log (tuoi);
+var x,y,z;// khai bao nhieu bien
+x = 10;
+y=20;
+z= x+y; // x+y la ,ot bieu thuc
+var fullname = "le huu anh nhat";
+var price = 1000.10; // nmber:float ,dobule
+var ismarried = true; // boolean : true/false
+console.log (ismarried);
+console.log (fullname + tuoi);
+console.log (price);
+console.log(z);
+console.log(x>y);
+console.log(typeof z);
+console.log(typeof fullname);
+z = ++x + y;
